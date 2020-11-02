69d6326f3d46af6117eeaa3bbee10955e047d060
hsj <noylit11@gmail.com>
2020-11-02 14:38:06 +0900

    spring 개요 spring 프로젝트 설정 "spring  . IOC  . DI/DL" "실습  . 스프링 컨테이너" "실습  . bean 엘레멘트 속성"

---
+++ src/test/java/kr/or/ddit/confing/ioc/IocDITest.java
@@ -0,0 +1,109 @@
+package kr.or.ddit.confing.ioc;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.board.model.BoardVo;
+import kr.or.ddit.board.service.BoardService;
+import kr.or.ddit.board.service.BoardServiceI;
+import kr.or.ddit.person.model.Person;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:kr/or/ddit/config/spring/ioc/ioc.xml"})
+public class IocDITest {
+	
+	// ioc.xml을 바탕으로 스프링 빈이 잘 생성되었는지 확인
+	// setter-boardService, constructor-boardServiceC 주입 확인
+	
+	// DI   ioc.xml에서  DI(setter)방식 불러옴
+	// 스프링 빈중에 호환되는 타입의 빈이 있을때 주입
+	@Autowired	// ioc.xml내용 자동으로 불러옴
+	ApplicationContext context;
+	
+	@Resource(name="boardService") // bean id로 호출 
+	BoardService boardService;
+	BoardServiceI boardServiceI;
+	
+	@Resource(name="boardServiceC")	// bean id로 호출 
+	BoardService boardServiceC;
+	BoardServiceI boardServiceIC;
+	
+	
+	// boardService, boardServiceC 스프링빈에 주입한 boardRepository 스프링빈은
+	// 동일한 빈이므로 두 객체의 getter 메소드를 통해 얻어온 boardRepository 객체는 동일해야 한다
+	@Test
+	public void repositorySameTest() {
+		/***Given***/
+
+		
+		/***When***/
+
+		/***Then***/
+		assertEquals(boardService.getBoardRepository(), boardServiceC.getBoardRepository());
+	}
+	
+	@Test
+	public void DIAutowiredTest() {
+		/***Given***/
+
+		/***When***/
+
+		/***Then***/
+		assertNotNull(boardService);
+	}
+	
+	
+	// 스프링 컨테이너를 주입받아 DL을 통해 BoardService 스프링 빈이 제대로 생성되었는지 확인
+	@Test
+	public void DItest() {
+		/***Given***/
+		
+		/***When***/				// context 호출하면 DL -  xml.
+		BoardServiceI  boardService = context.getBean("boardService", BoardServiceI.class);
+		BoardVo boardVo = boardService.getBoard(1);
+		
+		/***Then***/
+		assertEquals("첫번째 글", boardVo.getTitle());
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Resource(name="person")
+	Person person;
+	
+	// person 스프링빈의 age(value), boardRepository(ref) 두 속성에 주입이 잘 되었는지 확인
+	@Test
+	public void valueRefTest() {
+		/***Given***/
+
+		/***When***/
+
+		/***Then***/
+		assertEquals(30, person.getAge());
+		assertEquals("내용", person.getBoardRepository().getBoard(1).getContent());
+	}
+	
+	
+	
+	
+	
+	
+}
